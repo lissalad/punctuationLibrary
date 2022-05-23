c6a1ac2236168de323fc62d85c0663a02efc1540
lissalad <elissalaymon@gmail.com>
2022-05-23 15:56:05 -0700

    run on sentence added ands

--- removePunctuation.js
+++ removePunctuation.js
@@ -1,17 +1,26 @@
-
 function removePunctuation(str) {
-  return str.replace(/[\.,-\/#!$%\^&\*';:{}=\-_`~()@\+\?><\[\]\+]/g, '');
+  return str.replace(/[\.,-\/#!$%\^&\*';:{}=\-_`~()@\+\?><\[\]\+]/g, "");
 }
 
-function removeSentenceEnds(str, runOn=false) {
-  if(runOn) {
-    return str.replace(/[!.?]/g, " and");
-  }
+function removeSentenceEnds(str) {
   return str.replace(/[!.?]/g, "");
 }
 
-// run on sentence
+function runOnSentence(str) {
+  str = str.replace(/[!.?]/g, " and");
+  let words = str.split(" ");
+  // console.log(words);
 
+  for (let i = 0; i < words.length; i += 1) {
+    if (words[i] === "and") {
+      if (words[i - 1] === "and" || words[i - 1] === "") {
+        words[i] = "";
+      }
+    }
+  }
+
+  return words.join(" ");
+}
 // keep apostrophes
 
 // smart capitalize
@@ -19,5 +28,9 @@ function removeSentenceEnds(str, runOn=false) {
 // contractions
 
 // console.log(removePunctuation("lissa's cool!new music"));
-console.log(removeSentenceEnds("this is really cool. i love it! do you think, or imagine, or hope, it's ok? no?!?!"));
-console.log(removeSentenceEnds("this is really cool. I love it! Do you think, or imagine, or hope, it's ok? No?!?!", true));
+// console.log(removeSentenceEnds("this is really cool. i love it! do you think, or imagine, or hope, it's ok? no?!?!"));
+console.log(
+  runOnSentence(
+    "this is really cool. I love it! Do you think, or imagine, or hope, it's ok? No?!?!"
+  )
+);
