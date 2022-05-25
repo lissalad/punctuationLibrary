b20be326da635c95c6f341f6faf40d575438ed5a
lissalad <elissalaymon@gmail.com>
2022-05-25 14:32:10 -0700

    keep apostrophes

--- removePunctuation.js
+++ removePunctuation.js
@@ -6,11 +6,17 @@ function removeSentenceEnds(str) {
   return str.replace(/[!.?]/g, "");
 }
 
-function runOnSentence(str) {
-  str = str.replace(/[!.?]/g, " and");
-  let words = str.split(" ");
-  // console.log(words);
+function keepApostrophes(str) {
+  return str.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, "");
+}
 
+function runOnSentence(str) { // IN PROGRESS
+  // replace punctuation with and, divide into array of words
+  let stripped = str.replace(/[!.?]/g, " and");
+  let words = stripped.split(" ");
+
+  // console.log(words);
+  // remove double "and"s
   for (let i = 0; i < words.length; i += 1) {
     if (words[i] === "and") {
       if (words[i - 1] === "and" || words[i - 1] === "") {
@@ -18,19 +24,35 @@ function runOnSentence(str) {
       }
     }
   }
+  
+  // remove any ADDED last "and"
+  str = str.split(" ");
 
+  // console.log(words[str.length-1]);
+  
+  if(words[words.length-1] === "and") {
+    console.log(words[words.length-1]);
+    if(str[str.length-1] !== "and") {
+      words[words.length-1] = "";
+    }
+  }
+  
   return words.join(" ");
 }
+
 // keep apostrophes
 
 // smart capitalize
 
 // contractions
 
+
+// -------------- TESTS ----------------- //
 // console.log(removePunctuation("lissa's cool!new music"));
 // console.log(removeSentenceEnds("this is really cool. i love it! do you think, or imagine, or hope, it's ok? no?!?!"));
-console.log(
-  runOnSentence(
-    "this is really cool. I love it! Do you think, or imagine, or hope, it's ok? No?!?!"
-  )
-);
+// console.log(
+//   runOnSentence(
+//     "this is really cool. I love it! Do you think, or imagine, or hope, it's ok? No?!?!"
+//   )
+// );
+console.log(keepApostrophes("!Lissa's +coo>l, wire+_"));
